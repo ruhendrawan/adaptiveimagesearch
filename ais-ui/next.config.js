f9ce09e2d547bf1766879aa4c910bdb89543c425
Rully Agus Hendrawan <ruhendrawan@pitt.edu>
2024-04-19 10:05:06 -0400

    fix next config

--- ais-ui/next.config.js
+++ ais-ui/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // basePath: '/ais-v1',
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["3.12.41.38:3000", "localhost:3000", "ais.personalized-learning.org"],
+        },
+    },
 }
 
 module.exports = nextConfig
