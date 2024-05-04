a094a0ff0dec6b3976433c45447a4adcbf5e4bdb
Rully Agus Hendrawan <ruhendrawan@pitt.edu>
2024-05-04 10:31:42 -0400

    fix image api

--- ais-ui/pages/api/image/[...slug].js
+++ ais-ui/pages/api/image/[...slug].js
@@ -6,31 +6,36 @@ import path from 'path';
 // https://github.com/vercel/next.js/discussions/18005
 // https://github.com/vercel/next.js/discussions/16417
 
+export const config = {
+    api: {
+        externalResolver: true,
+    },
+}
+
 export default function handler(req, res) {
     if (req.query.slug && req.query.slug.length) {
-        
+
         try {
             const fileUrl = req.query.slug.join("/")
             const filePath = path.join(process.cwd(), 'public/images', fileUrl);
-            // console.debug(filePath);
+            // console.log(filePath);
             fs.readFile(filePath, (err, data) => {
                 if (err) {
                     if (err.code === 'ENOENT') {
-                        res.status(404).json({ error: 'File not found' });
-                        return;
+                        return res.status(404).json({ error: 'File not found' });
                     } else {
-                        res.status(500).json({ error: 'Failed to read file' });
-                        return;
+                        return res.status(500).json({ error: 'Failed to read file' });
+
                     }
                 } else {
                     res.setHeader("Content-Type", "image/jpg");
-                    return res.status(200).send(data)
+                    return res.status(200).send(data);
                 }
             })
         } catch (error) {
             return res.status(500).json({ error: 'Failed to fetch file' });
         }
     } else {
-        return res.status(404).send(null)
+        return res.status(404).send(null);
     }
 }
